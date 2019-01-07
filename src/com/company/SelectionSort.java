310bb19885727b394c5967f1d39afa2e4244d687
BT_1N3_04@bktech.local <BT_1N3_04@bktech.local>
2019-01-07 08:40:34 -0500

    Initial commit

---
+++ src/com/company/SelectionSort.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class SelectionSort {
+    public static void Swap(int[] arr, int i, int j) {
+        int t = arr[i];
+        arr[j] = arr[i];
+        arr[j] = t;
+    }
+    public static void CompareValues(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1; j++) {
+                if (arr[i] < arr[j + 1]) {
+
+
+                }
+            }
+        }
+    }
+}
