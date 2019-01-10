2a755b3b6820e857fcd7b51c06e6920d102e8962
yhuang7593 <yhuang7593@bths.edu>
2019-01-10 01:29:56 -0500

    new

--- src/com/company/SelectionSort.java
+++ src/com/company/SelectionSort.java
@@ -2,18 +2,16 @@ package com.company;
 
 public class SelectionSort {
     public static void selectionSort(int[] arr) {
-        for (int i=0;i<arr.length; i++){
-            int minIndex=i;
-            for (int j=i+1;j<arr.length;j++){
-                if (arr[j]<arr[minIndex]){
-                    j=minIndex;
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
                 }
             }
-            if(minIndex!=i){
-                int temp=arr[i];
-                arr[i]=arr[minIndex];
-                arr[minIndex]=temp;
-            }
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
         }
     }
 }
